1738594437a9d51afcd471ef2c0646f69fb9a215
Ben Morrow <b.morrow@amrc.co.uk>
2023-06-22 16:03:26 +0100

    Adjust the change-password portion.
    We already know the username to use. Change with chpasswd(1), as this
    doesn't bypass PAM. Clear the password from the screen afterwards.

--- sh/change-password.sh
+++ sh/change-password.sh
@@ -1,9 +1,11 @@
 # ACS Cell Gateway bootstrap script
 # Set an admin password
 
-read -p "Enter admin user to set password for: " user
-
 echo Changing admin password...
-#PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
-#usermod -p $(openssl passwd -1 $PASSWORD) "$user"
-#echo "Password set to: $PASSWORD"
+(
+    PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
+    #echo "${SUDO_USER}:${PASSWORD}" | chpasswd
+    echo "Password for ${SUDO_USER} set to: $PASSWORD"
+    read -p "Press enter WHEN THIS HAS BEEN RECORDED:" dummy
+    tput cuu1 cuu1 ed
+)
