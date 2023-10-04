2b1b36f4d942865c73eefec00925ed436fcd4f7c
Ben Morrow <b.morrow@amrc.co.uk>
2023-10-04 11:13:40 +0100

    Install sealed-secrets into the right namespace.
    Otherwise the Helm chart can't take ownership.

--- sh/kubeseal.sh
+++ sh/kubeseal.sh
@@ -3,7 +3,9 @@
 
 echo Configuring Sealed Secrets...
 helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
-helm install sealed-secrets sealed-secrets/sealed-secrets
+helm install \
+    -n sealed-secrets --create-namespace \
+    sealed-secrets sealed-secrets/sealed-secrets
 tar -xvzf ./install/kubeseal.tar.gz -C install kubeseal
 
 echo "Fetching sealed secrets certificate..."
@@ -12,7 +14,7 @@ sleep 3
 while true
 do
   ./install/kubeseal --controller-name sealed-secrets \
-    --controller-namespace default \
+    --controller-namespace sealed-secrets \
     --fetch-cert >./install/kubesealCert.pem \
     || true
 
